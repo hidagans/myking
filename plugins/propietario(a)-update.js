6a3a0f462902a41a87c091296ecc37cf6cfe3638
hidagans <58388773+hidagans@users.noreply.github.com>
2023-10-03 03:53:48 +0700

    Update propietario(a)-update.js

--- plugins/propietario(a)-update.js
+++ plugins/propietario(a)-update.js
@@ -5,7 +5,7 @@ if (global.conn.user.jid == conn.user.jid) {
 let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
 conn.reply(m.chat, stdout.toString(), m)}
 //} catch {
-var update = execSync('git remote set-url origin https://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@github.com/hidagans/myking.git && git pull')
+var update = execSync('git pull')
 await m.reply(update.toString())
 } catch {
 await m.reply(`${fg}`) 
