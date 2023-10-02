b6a1094080eba7bc5442f408a72d3eae10cea4bc
hidagans <58388773+hidagans@users.noreply.github.com>
2023-10-03 02:21:01 +0700

    Update propietario(a)-update.js

--- plugins/propietario(a)-update.js
+++ plugins/propietario(a)-update.js
@@ -5,7 +5,7 @@ if (global.conn.user.jid == conn.user.jid) {
 let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
 conn.reply(m.chat, stdout.toString(), m)}
 //} catch {
-var update = execSync('git remote set-url origin https://github.com/elrebelde21/The-LoliBot-MD.git && git pull')
+var update = execSync('git remote set-url origin https://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@github.com/hidagans/myking.git && git pull')
 await m.reply(update.toString())
 } catch {
 await m.reply(`${fg}`) 
