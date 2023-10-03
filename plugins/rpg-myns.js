71cb1e5cf1bc87af981c95482441faa0142c9b68
hidagans <58388773+hidagans@users.noreply.github.com>
2023-10-04 00:01:36 +0700

    Update rpg-myns.js

--- plugins/rpg-myns.js
+++ plugins/rpg-myns.js
@@ -1,7 +1,7 @@
 import { createHash } from 'crypto'
 let handler = async function (m, { conn, text, usedPrefix }) {
 let sn = createHash('md5').update(m.sender).digest('hex')
-await m.reply(`⬇️ *ᴇsᴛᴇ ᴇs sᴜs ɴᴜᴍᴇʀᴏ ᴅᴇʟ sᴇʀɪᴇ * ⬇️`.trim())
+await m.reply(`⬇️ *ini adalah nomor seri Anda * ⬇️`.trim())
 await m.reply(`${sn}`.trim())
 }
 handler.help = ['myns']
