81748685c915848fd408e980a3a071c96735ea43
hidagans <58388773+hidagans@users.noreply.github.com>
2023-10-03 23:17:24 +0700

    Update rpg-colecciones.js

--- plugins/rpg-colecciones.js
+++ plugins/rpg-colecciones.js
@@ -1,27 +1,27 @@
 let handler = async (m, { command, usedPrefix, DevMode, args, conn }) => {
-const valoracion = { valorar1: 'Muy Alta 80% - 100%', valorar2: 'Alta 60% - 79%', valorar3: 'Media 40% - 59%', valorar4: 'Baja 20% - 39%', valorar5: 'Muy Baja 0% - 19%' }
+const valoracion = { valorar1: 'Sangat Tinggi 80% - 100%', valorar2: 'Tinggi 60% - 79%', valorar3: 'Media 40% - 59%', valorar4: 'Rendah 20% - 39%', valorar5: 'Sangat Rendah 0% - 19%' }
 
 const categorias = {
 exp: {
-categoria: 'Categoria 1',
-descripcion: 'Experiencia',
+categoria: 'Category 1',
+descripcion: 'Pengalaman',
 ataque: '',
 defensa: '',
 utilidad: valoracion.valorar1,
 abundancia: valoracion[0]
 },
 stamina: {
-categoria: 'Categoria 2',
-descripcion: 'Energia',
+categoria: 'Categori 2',
+descripcion: 'Energi',
 ataque: '',
 defensa: '',
-utilidad: 'Medianamente alta',
-abundancia: 'Medianamente poca' 
+utilidad: 'Sedang tinggi',
+abundancia: 'Cukup sedikit' 
 }
 }
 m.reply(categorias.exp.utilidad)
 }
-handler.command = /^(colección|coleccion|inforpg|set|collection)$/i
+handler.command = /^(collect|coleccion|inforpg|set|collection)$/i
 handler.register = true
 
 export default handler
