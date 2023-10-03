ec9b4f6e0123dc777897e5f9dde76ef132d26cac
hidagans <58388773+hidagans@users.noreply.github.com>
2023-10-03 22:31:45 +0700

    Update rpg-añadir.Diamantes.js

--- plugins/rpg-añadir.Diamantes.js
+++ plugins/rpg-añadir.Diamantes.js
@@ -4,25 +4,25 @@ let handler = async (m, { conn, text }) => {
 let who
 if (m.isGroup) who = m.mentionedJid[0]
 else who = m.chat
-if (!who) throw `${ag}𝘿𝙀𝘽𝙀 𝘿𝙀 𝙀𝙏𝙄𝙌𝙐𝙀𝙏𝘼𝙍 𝘼𝙇 𝙐𝙎𝙐𝘼𝙍𝙄𝙊 *@tag*`
+if (!who) throw `${ag}harus menandai pengguna *@tag*`
 let txt = text.replace('@' + who.split`@`[0], '').trim()
-if (!txt) throw `${ag}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙇𝘼 𝘾𝘼𝙉𝙏𝙄𝘿𝘼𝘿 𝘿𝙀 𝘿𝙄𝘼𝙈𝘼𝙉𝙏𝙀𝙎`
-if (isNaN(txt)) throw `${mg}𝙎𝙄𝙉 𝙎𝙄𝙈𝘽𝙊𝙇𝙊𝙎, 𝙎𝙊𝙇𝙊 𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙉𝙐𝙈𝙀𝙍𝙊𝙎`
+if (!txt) throw `${ag}masukkan jumlah Diamond`
+if (isNaN(txt)) throw `${mg}tanpa simbol, cukup masukkan angka`
 let dmt = parseInt(txt)
 let limit = dmt
 let pjk = Math.ceil(dmt * pajak)
 limit += pjk
-if (limit < 1) throw `${mg}𝙀𝙇 𝙉𝙐𝙈𝙀𝙍𝙊 𝙈𝙄𝙉𝙄𝙈𝙊 𝙋𝘼𝙍𝘼 𝘿𝙄𝘼𝙈𝘼𝙉𝙏𝙀𝙎 𝙀𝙎 *1*`
+if (limit < 1) throw `${mg}jumlah minimum untuk Diamond adalah *1*`
 let users = global.db.data.users
 users[who].limit += dmt
-m.reply(`*╭[ 💎 Diamantes 💎 ]⬣*\n┃\n┃ღ *Para:*\n┃ღ *${text}*\n┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n┃ღ *Se le añadio*\n┃ღ *${dmt} Diamante(s)* 💎\n┃\n* ╰━━━━━━━━━━━━━━⬣*`)}
+m.reply(`*╭[ 💎 Diamond 💎 ]⬣*\n┃\n┃ღ *Untuk:*\n┃ღ *${text}*\n┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n┃ღ *Telah ditambahkan*\n┃ღ *${dmt} Diamonds(s)* 💎\n┃\n* ╰━━━━━━━━━━━━━━⬣*`)}
 /*conn.sendHydrated(m.chat, `╭[ 𝘿𝙄𝘼𝙈𝘼𝙉𝙏𝙀𝙎 | 𝘿𝙄𝘼𝙈𝙊𝙉𝘿 💎 ]⬣\n┃\n┃ღ *PARA | FOR:*\n┃ღ *${text}*\n┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n┃ღ *SE LE AÑADIÓ | NOW YOU HAVE*\n┃ღ *${dmt} Diamante(s)* 💎\n┃\n╰━━━━━━━━━━━━━━⬣`, wm, null, md, '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
 ['💗 𝙈𝙚𝙣𝙪 𝘼𝙫𝙚𝙣𝙩𝙪𝙧𝙖 | 𝙍𝙋𝙂 💗', '.rpgmenu'],
 ['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']], m)
 }*/
 handler.help = ['adddi <@user>']
 handler.tags = ['xp']
-handler.command = ['añadirdiamantes', 'dardiamantes', 'dardiamante'] 
+handler.command = ['tfdm', 'adddm', 'tfdiamond'] 
 handler.group = true
 handler.rowner = true
 export default handler
