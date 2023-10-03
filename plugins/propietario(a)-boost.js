49025aec31c7ecb0f0a33d6856e4b741e11e5169
hidagans <58388773+hidagans@users.noreply.github.com>
2023-10-03 22:13:21 +0700

    Update propietario(a)-boost.js

--- plugins/propietario(a)-boost.js
+++ plugins/propietario(a)-boost.js
@@ -1,6 +1,6 @@
 import { performance } from 'perf_hooks'
 let handler = async (m, { conn }) => {
- let start = `Aceleración de inicio... Espere un momento`
+ let start = `Akselerasi start-up... Tunggu sebentar`
  let boost = `${pickRandom(['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'])}%`
  let boost2 = `${pickRandom(['21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40'])}%`
  let boost3 = `${pickRandom(['41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60'])}%`
@@ -19,7 +19,7 @@ await conn.sendMessage(m.chat, {text: boost5, edit: key});
    let old = performance.now()
    let neww = performance.now()
    let speed = `${neww - old}`
-   let finish = `*_✔️ Bot acelerado con exito_*\n\n*_Velocidad: ${speed} milisegundos!_*`
+   let finish = `*_✔️ Bot berhasil dipercepat_*\n\n*_Kecepatan: ${speed} milisecond!_*`
 await conn.sendMessage(m.chat, {text: finish, edit: key});
 //conn.reply(m.chat, finish, m)
 }
