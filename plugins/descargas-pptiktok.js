abd77295d5ed6fe160ce283ebf0fb49f7b934e08
hidagans <58388773+hidagans@users.noreply.github.com>
2023-10-05 10:09:55 +0700

    Update descargas-pptiktok.js

--- plugins/descargas-pptiktok.js
+++ plugins/descargas-pptiktok.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args, text, command, usedPrefix }) => {
 if (!text) throw `𝙀𝙨𝙘𝙧𝙞𝙗𝙖 𝙚𝙡 𝙣𝙤𝙢𝙗𝙧𝙚 𝙙𝙚 𝙪𝙨𝙪𝙖𝙧𝙞𝙤 𝙙𝙚 𝙩𝙞𝙠𝙩𝙤𝙠
 𝙎𝙞𝙣 𝙪𝙨𝙖𝙧 "@"\n𝙀𝙟𝙚𝙢𝙥𝙡𝙤\n*${usedPrefix + command} mundo_dos_animes81*`
 let res = `https://api.lolhuman.xyz/api/pptiktok/${text}?apikey=9b817532fadff8fc7cb86862`
-await conn.sendFile(m.chat, res, 'error.jpg', `✅ 𝙁𝙤𝙩𝙤 𝙙𝙚 𝙥𝙚𝙧𝙛𝙞𝙡*${text}*`, m, false)
+await conn.sendFile(m.chat, res, 'error.jpg', `✅ Foto profile*${text}*`, m, false)
   
 }
 handler.help = ['tiktokfoto'].map(v => v + ' <username>')
