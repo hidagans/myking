f20c8a464924b732da6592ae27601f15f3f1ea31
hidagans <58388773+hidagans@users.noreply.github.com>
2023-10-05 10:30:02 +0700

    Update grupo-configuracion.js

--- plugins/grupo-configuracion.js
+++ plugins/grupo-configuracion.js
@@ -3,39 +3,39 @@ const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './
 const { isBanned, autolevelup, antiver, antitoxic, temporal, restrict, stickers, welcome, detect, sWelcome, sBye, sPromote, sDemote, antiLink, antiLink2, modohorny, autosticker, audios, delete: del } = global.db.data.chats[m.chat]
 
 let text = 
-`╭━[ 𝘾𝙤𝙣𝙛𝙞𝙜𝙪𝙧𝙖𝙘𝙞𝙤́𝙣 ]━⬣
+`╭━[ KONFIGURASI ]━⬣
 ┃
-┃ 𝙒𝙚𝙡𝙘𝙤𝙢𝙚 ${welcome ? '✅' : '❌'}
+┃ Welcome ${welcome ? '✅' : '❌'}
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ 𝘼𝙣𝙩𝙞𝙡𝙞𝙣𝙠 ${antiLink ? '✅' : '❌'} 
+┃ Antilink ${antiLink ? '✅' : '❌'} 
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ 𝘼𝙣𝙩𝙞𝙡𝙞𝙣𝙠 *2* ${antiLink2 ? '✅' : '❌'} 
+┃ Antilink *2* ${antiLink2 ? '✅' : '❌'} 
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ 𝙎𝙩𝙞𝙘𝙠𝙚𝙧 ${stickers ? '✅' : '❌'}
+┃ Stickers ${stickers ? '✅' : '❌'}
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ 𝙁𝙪𝙣𝙘𝙞𝙤𝙣 𝙗𝙤𝙩𝙚𝙢𝙥𝙤𝙧𝙖𝙡  ${global.db.data.settings[conn.user.jid].temporal ? '✅' : '❌'}
+┃ Fungsi sementara  ${global.db.data.settings[conn.user.jid].temporal ? '✅' : '❌'}
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ 𝙁𝙪𝙣𝙘𝙞𝙤𝙣 𝙖𝙣̃𝙖𝙙𝙞𝙧 𝙮 𝙨𝙖𝙘𝙖 ${global.db.data.settings[conn.user.jid].restrict ? '✅' : '❌'}
+┃ fungsi tambah dan hapus ${global.db.data.settings[conn.user.jid].restrict ? '✅' : '❌'}
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ 𝘼𝙪𝙩𝙤𝙡𝙚𝙫𝙚𝙡𝙪𝙥 ${global.db.data.users[m.sender].autolevelup ? '✅' : '❌'}
+┃ Autolevelup ${global.db.data.users[m.sender].autolevelup ? '✅' : '❌'}
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ 𝘿𝙚𝙩𝙚𝙘𝙩 ${detect ? '✅' : '❌'} 
+┃ Detect ${detect ? '✅' : '❌'} 
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ 𝘼𝙣𝙩𝙞𝙩𝙤𝙭𝙞𝙘 ${antitoxic ? '✅' : '❌'} 
+┃ Antitoxic ${antitoxic ? '✅' : '❌'} 
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ 𝙖𝙣𝙩𝙞𝙫𝙞𝙚𝙬𝙤𝙣𝙘𝙚 ${antiver ? '✅' : '❌'}
+┃ Antiviewonce ${antiver ? '✅' : '❌'}
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ 𝘼𝙣𝙩𝙞𝙙𝙚𝙡𝙚𝙩𝙚 ${global.db.data.chats[m.chat].delete ? '✅' : '❌'} 
+┃ Antidelete ${global.db.data.chats[m.chat].delete ? '✅' : '❌'} 
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ 𝙈𝙤𝙙𝙤 𝙝𝙤𝙧𝙣𝙮 (+18) ${modohorny ? '✅' : '❌'} 
+┃ Mode Horny (+18) ${modohorny ? '✅' : '❌'} 
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ 𝘼𝙪𝙩𝙤𝙨𝙩𝙞𝙘𝙠𝙚𝙧 ${autosticker ? '✅' : '❌'} 
+┃ Autosticker${autosticker ? '✅' : '❌'} 
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ 𝘼𝙪𝙙𝙞𝙤𝙨 ${audios ? '✅' : '❌'} 
+┃ Audios ${audios ? '✅' : '❌'} 
 ╰━━━━━❰ *𓃠 ${vs}* ❱━━━━⬣
 `.trim()
 //conn.sendFile(m.chat, pp, 'error.jpg', text, m, false, { mentions: [...groupAdmins.map(v => v.id), owner] }) 
-conn.sendHydrated(m.chat, text, wm, pp, md, '𝑻𝒉𝒆 𝑳𝒐𝒍𝒊𝑩𝒐𝒕-𝑴𝑫', null, null, [
+conn.sendHydrated(m.chat, text, wm, pp, md, 'MajestyBot', null, null, [
 ['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ ☘️', '/menu']
 ], m,)
 }
