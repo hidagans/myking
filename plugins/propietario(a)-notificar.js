3aad5614d463a662913e242f3f5e2f345f3f3a33
hidagans <58388773+hidagans@users.noreply.github.com>
2023-10-05 10:51:31 +0700

    Update propietario(a)-notificar.js

--- plugins/propietario(a)-notificar.js
+++ plugins/propietario(a)-notificar.js
@@ -8,20 +8,20 @@ let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status
 let [_, code] = grupo.match(linkRegex) || []
 
 if ( users == 5492266613038 || users == 593968585383 || users == 593993684821  || users == 59894808483 || users == 5492266466080 ) try {
-if (!text) return m.reply(`*Falta Texto*`) 
+if (!text) return m.reply(`*Teks salah.*`) 
 await delay(5 * 5000)
 let res = await conn.groupAcceptInvite(code)
 await delay(5 * 5000)
 await conn.sendMessage(res, { text: text + ( users == 593993684821 ? '\n\n_atte. 𝗚𝗔𝗧𝗔 𝗗𝗜𝗢𝗦_' : '' || users == 593968585383 ? '\n\n_atte. 𝗚𝗔𝗧𝗔 𝗗𝗜𝗢𝗦_' : '' || users ==59894808483 ? '\n\n_atte. 𝑨𝒛𝒂𝒎𝒊❤_' : '' || users ==5492266466080 ? '\n\n_atte. 𝙇𝙤𝙡𝙞𝘽𝙤𝙩-𝙈𝘿_' : '' || users == 5492266613038 ? '\n\n_atte. 𝙇𝙤𝙡𝙞𝘽𝙤𝙩-𝙈𝘿_' : '' ), mentions: (await conn.groupMetadata(`${res}`)).participants.map(v => v.id) }, { quoted: fkontak })
-await m.reply(`✅ *MENSAJE ENVIADO CON ÉXITO* `)
+await m.reply(`✅ *PESAN BERHASIL TERKIRIM* `)
 
 } catch (e) {
-await conn.sendButton(m.chat, `\n${wm}`, lenguajeGB['smsMalError3']() + '#report ' + usedPrefix + command, null, [[lenguajeGB.smsMensError1(), `#reporte ${lenguajeGB['smsMensError2']()} *${usedPrefix + command}*`]], m)
+await conn.sendButton(m.chat, `\n${wm}`, lenguajeGB['smsMalError3']() + '#report ' + usedPrefix + command, null, [[lenguajeGB.smsMensError1(), `#lapor ${lenguajeGB['smsMensError2']()} *${usedPrefix + command}*`]], m)
 console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
 console.log(e)
 
 } else {
-await m.reply('```USTED NO TIENE AUTORIZACIÓN PARA USAR ESTE COMANDO.```')}
+await m.reply('```ANDA TIDAK BERWENANG UNTUK MENGGUNAKAN PERINTAH INI.```')}
 }
 handler.command = ['mensajeoficial']
 handler.owner = true
