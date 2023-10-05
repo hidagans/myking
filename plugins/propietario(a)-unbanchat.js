554fc23543c8a463cac627a92a7f23d8650f50da
hidagans <58388773+hidagans@users.noreply.github.com>
2023-10-05 10:52:34 +0700

    Update propietario(a)-unbanchat.js

--- plugins/propietario(a)-unbanchat.js
+++ plugins/propietario(a)-unbanchat.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
-  if (!(m.chat in global.db.data.chats)) return m.reply('*Este chat no está registrado en la base de datos!*')
+  if (!(m.chat in global.db.data.chats)) return m.reply('*Obrolan ini tidak terdaftar dalam database.!*')
   let chat = global.db.data.chats[m.chat]
-  if (!chat.isBanned) return m.reply('*Este chat no está baneado!!*')
+  if (!chat.isBanned) return m.reply('*Obrolan ini tidak diblokir!!*')
   chat.isBanned = false
   m.reply(`*BOT ONLINE 🚀*`)
 }
